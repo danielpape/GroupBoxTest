55831fad239918b228b99ad62fe4de0bf7140320
Daniel Pape <danielwpape@gmail.com>
2021-07-30 20:41:17 +0100

    Initial Commit

---
+++ GroupBox/GroupBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxApp.swift
+//  GroupBox
+//
+//  Created by Daniel Pape on 30/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
